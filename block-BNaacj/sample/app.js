eca20118e48d563e7064838190437e3287ef98b6
lovekushrajput <lovekushrazput143@gmail.com>
2022-04-03 14:23:22 +0530

    block-BNaacj

---
+++ block-BNaacj/sample/app.js
@@ -0,0 +1,15 @@
+let express = require('express')
+let mongoose = require('mongoose')
+
+let app = express()
+
+mongoose.connect('mongodb://localhost/sample',(err)=>{
+    console.log( err ? err : 'connected to sample dbs')
+})
+app.get('/',(req,res)=>{
+    res.send('Welcome')
+})
+
+app.listen(4000,()=>{
+    console.log('server is listening on port 4k')
+})
