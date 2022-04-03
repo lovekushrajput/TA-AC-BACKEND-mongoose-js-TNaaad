cbb4c35d5a3308866b89bbb1c8572a2da7bb45de
lovekushrajput <lovekushrazput143@gmail.com>
2022-04-03 13:53:15 +0530

    changes

--- block-BNaacf/sample/models/user.js
+++ block-BNaacf/sample/models/user.js
@@ -2,15 +2,12 @@ let mongoose = require('mongoose')
 let Schema = mongoose.Schema
 
 let userSchema = new Schema({
+    name : String ,
+    email : {type : String, lowercase:true},
+    age : {type : Number ,default :0},
     favorites : [String],
     marks : [Number]
 },{timestamps : true})
 
-let addressSchema = new Schema({
-    village : String,
-    city : String,
-    state : String,
-    pin : Number,
-    author : Schema.Types.ObjectId
-})
+
  
